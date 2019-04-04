e0ffbe5e6e2b67c420418fd194828f6c3b536dde
shortendara <dara.shorten@gmail.com>
2019-04-04 12:52:22 +0100

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t shortendara/multi-client:latest -t shortendara/multi-client:$SHA  -f ./client/Dockerfile ./client
+docker build -t shortendara/multi-server:latest -t shortendara/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t shortendara/multi-worker:latest -t shortendara/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push shortendara/multi-server:latest
+docker push shortendara/multi-client:latest
+docker push shortendara/multi-worker:latest
+
+docker push shortendara/multi-server:$SHA
+docker push shortendara/multi-client:$SHA
+docker push shortendara/multi-worker:$SHA
+
+kubectl apply -f ./k8s
+kubectl set image deployments/client-deployment client=shortendara/multi-client:$SHA
+kubectl set image deployments/server-deployment server=shortendara/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=shortendara/multi-worker:$SHA
